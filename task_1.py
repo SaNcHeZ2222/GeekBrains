2870e4e951dd877f265c2c703388467d45bc409c
SaNcHeZ2222 <86947367+SaNcHeZ2222@users.noreply.github.com>
2022-10-06 01:04:37 +0300

    Lesson_2

--- task_1.py
+++ task_1.py
@@ -1,73 +1,31 @@
-"""
-Задание 1.
-
-Для каждой из трех функций выполнить следующее:
-
-1) для каждого выражения вместо символов !!! укажите сложность.
-2) определите сложность алгоритма в целом (Сложность: !!!).
-
-Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
--- Сложность нужно указать только там, где есть !!!
--- Сложности встроенных функций и операций нужно искать
-    в таблицах (см. материалы к уроку).
-"""
-
-from random import sample
-
-
-##############################################################################
-def check_1(lst_obj):
-    """Функция должна создать множество из списка.
-
-    Алгоритм 1:
-    Создать множество из списка
-
-    Сложность: O(len(lst_obj))
-    """
-    lst_to_set = set(lst_obj)  # O(len(lst_obj))
-    return lst_to_set  # O(1)
-
-
-##############################################################################
-def check_2(lst_obj):
-    """Функция должная вернуть True, если все элементы списка различаются.
-
-    Алгоритм 2:
-    Проходимся по списку и для каждого элемента проверяем,
-    что такой элемент отстутствует
-    в оставшихся справа элементах
-
-    Сложность: O(n ** 2).
-    """
-    for j in range(len(lst_obj)):          # O(n)
-        if lst_obj[j] in lst_obj[j+1:]:    # O(n)
-            return False                   # 1
-    return True                            # 1
+def calculator():
+    symbol = input('Введите операцию (+, -, *, / или 0 для выхода): ')
+    if symbol == '0':
+        return
+    elif symbol not in '+-*/':
+        print('Проблема, попробуй ещё раз ввести символ')
+        return calculator()
+    number_1, number_2 = input('Введите два числа через пробел: ').split()
+    if not number_1.isdigit() or not number_2.isdigit():
+        print('Введите число, а не строку')
+        return calculator()
+    number_1 = int(number_1)
+    number_2 = int(number_2)
+    if symbol == '+':
+        print(number_1 + number_2)
+        return calculator()
+    elif symbol == '*':
+        print(number_1 * number_2)
+        return calculator()
+    elif symbol == '-':
+        print(number_1 - number_2)
+        return calculator()
+    elif symbol == '/':
+        if number_2 == 0:
+            print('Делить на ноль нельзя')
+            return calculator()
+        print(number_1 / number_2)
+        return calculator()
 
 
-##############################################################################
-def check_3(lst_obj):
-    """Функция должная вернуть True, если все элементы списка различаются.
-
-    Алгоритм 3:
-    Вначале выполним для списка сортировку, далее, сравниваем элементы попарно
-    Если присутствуют дубли, они будут находиться рядом.
-
-    Сложность: O(n ** 2)
-    """
-    lst_copy = list(lst_obj)                 # O(n)
-    lst_copy.sort()                          # O(n log n)
-    for i in range(len(lst_obj) - 1):        # O(n)
-        if lst_copy[i] == lst_copy[i+1]:     # 1
-            return False                     # 1
-    return True                              # 1
-
-
-for j in (50, 500, 1000, 5000, 10000):
-    # Из 100000 чисел возьмем 'j' случайно выбранных
-    # Всего 10 тыс. чисел
-    lst = sample(range(-100000, 100000), j)
-
-print(check_1(lst))
-print(check_2(lst))
-print(check_3(lst))
+calculator()
