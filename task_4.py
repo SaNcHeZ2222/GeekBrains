84f08fcf2acb75ef41da8c9f3899c019a85ae73d
SaNcHeZ2222 <86947367+SaNcHeZ2222@users.noreply.github.com>
2022-10-06 00:57:35 +0300

    Add files via upload

--- task_4.py
+++ task_4.py
@@ -1,58 +1,11 @@
-print("Пример 1")
+def sum_n_order(n, elem=1, count=1):
+    if n == 1:
+        print(count)
+        return
+    elem = elem / -2
+    count += elem
+    n -= 1
+    return sum_n_order(n, elem, count)
 
 
-def check_password(password):
-    if len(password) < 6: # 1
-        return 0
-    else:
-        return 1
-
-
-users = {}
-
-while True:
-    login, password = input('Введите логин и пароль через пробел: ').split() # 1
-    if login in users.keys(): # 1
-        print('Увы, этот логин уже занят')
-    else:
-        break
-
-while True:
-    if check_password(password) == 0: # 1
-        password = input('Ваш пароль слишком простой, введите новый: ') # 1
-    else:
-        print('Отлично, вы успешно зарегистрировались')
-        users[login + '_+'] = password # _+ отметка об активации # 1
-        print(users)
-        break
-
-# 1 общая сложность решения
-
-print('Пример 2')
-
-
-def check_password(password):
-    for i in password: # n
-        if ord(i) < 65: # Начиная с заглавных английских 1
-            return 0
-    return 1
-
-users = {}
-
-while True:
-    login, password = input('Введите логин и пароль через пробел: ').split() # 1
-    if login in users.keys(): # 1
-        print('Увы, этот логин уже занят')
-    else:
-        break
-
-while True:
-    if check_password(password) == 0: # 1
-        password = input('Ваш пароль слишком простой, введите новый: ') # 1
-    else:
-        print('Отлично, вы успешно зарегистрировались')
-        users[login + '_+'] = password # _+ отметка об активации # 1
-        print(users)
-        break
-
-# n общая сложность решения
+sum_n_order(int(input('Введите длину последовательности: ')))
--- task_5.py
+++ task_5.py
@@ -1,44 +1,10 @@
-class CreatePilePlate:
-    def __init__(self, max_len):
-        self.pile = [[]]
-        self.number = 0
-        self.len = max_len
-
-    def add_plate(self, count):
-        for _ in range(count):
-            self.pile[self.number].append(1)
-            if len(self.pile[self.number]) == self.len:
-                self.pile.append([])
-                self.number += 1
-        print(self.pile)
-
-    def add_plate_in_new_list(self, count):
-        self.pile.append([])
-        self.number += 1
-        for _ in range(count):
-            self.pile[self.number].append(1)
-            if len(self.pile[self.number]) == self.len:
-                self.pile.append([])
-                self.number += 1
-        print(self.pile)
-
-    def delete_pile(self, count):
-        index = len(self.pile) - 1
-        while True:
-            try:
-                self.pile[index].pop()
-                count -= 1
-            except Exception as e:
-                del self.pile[index]
-                index -= 1
-            if count == 0:
-                return
+def go_matrix_ascii(number=32, count=0):
+    if number == 128:
+        return
+    if count % 10 == 0:
+        print()
+    print(f'{number} - {chr(number)}', end = ' ')
+    go_matrix_ascii(number + 1, count + 1)
 
 
-pile = CreatePilePlate(4)
-pile.add_plate(3)
-pile.add_plate_in_new_list(5)
-pile.add_plate(3)
-pile.add_plate_in_new_list(5)
-pile.delete_pile(6)
-print(pile.pile)
+go_matrix_ascii()
