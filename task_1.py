f15a75f2f49c28521f2044fd7e521083251e7366
SaNcHeZ2222 <86947367+SaNcHeZ2222@users.noreply.github.com>
2022-10-07 13:13:29 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -6,25 +6,26 @@ def calculator():
         print('Проблема, попробуй ещё раз ввести символ')
         return calculator()
     number_1, number_2 = input('Введите два числа через пробел: ').split()
-    if not number_1.isdigit() or not number_2.isdigit():
-        print('Введите число, а не строку')
-        return calculator()
-    number_1 = int(number_1)
-    number_2 = int(number_2)
-    if symbol == '+':
-        print(number_1 + number_2)
-        return calculator()
-    elif symbol == '*':
-        print(number_1 * number_2)
-        return calculator()
-    elif symbol == '-':
-        print(number_1 - number_2)
-        return calculator()
-    elif symbol == '/':
-        if number_2 == 0:
-            print('Делить на ноль нельзя')
+    try:
+        number_1 = int(number_1)
+        number_2 = int(number_2)
+        if symbol == '+':
+            print(number_1 + number_2)
             return calculator()
-        print(number_1 / number_2)
+        elif symbol == '*':
+            print(number_1 * number_2)
+            return calculator()
+        elif symbol == '-':
+            print(number_1 - number_2)
+            return calculator()
+        elif symbol == '/':
+            if number_2 == 0:
+                print('Делить на ноль нельзя')
+                return calculator()
+            print(number_1 / number_2)
+            return calculator()
+    except ValueError:
+        print('Введите число, а не строку')
         return calculator()
 
 
