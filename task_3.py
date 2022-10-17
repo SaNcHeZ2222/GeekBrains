17b3c1992812806ae7b8c3582653681437716c09
SaNcHeZ2222 <86947367+SaNcHeZ2222@users.noreply.github.com>
2022-10-17 21:37:44 +0300

    Lesson_4

--- task_3.py
+++ task_3.py
@@ -1,12 +1,94 @@
-def coup(digit, rest=''):
-    if digit == 0:
-        print(rest)
-        return
-    last_elem = str(digit % 10)
-    digit = digit // 10
-    rest += last_elem
-    coup(digit, rest)
+"""
+Задача 3.
+В соответствии с документацией Python,
+deque – это обобщение стеков и очередей.
+Вот основное правило: если вам нужно
+что-то быстро дописать или вытащить, используйте deque.
+Если вам нужен быстрый случайный доступ, используйте list
+Задача: создайте простой список (list) и очередь (deque).
+Выполните различные операции с каждым из объектов.
+Сделайте замеры и оцените, насколько информация в документации
+соответствует дейстивтельности.
+1) сравнить операции
+append, pop, extend списка и дека и сделать выводы что и где быстрее
+2) сравнить операции
+appendleft, popleft, extendleft дека и соответствующих им операций списка
+и сделать выводы что и где быстрее
+3) сравнить операции получения элемента списка и дека
+и сделать выводы что и где быстрее
+Подсказка:
+для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
+(append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
+"""
+from collections import deque
+from timeit import timeit
+
+list_1 = []
+
+deq = deque()
 
 
-coup(int(input('Введите число: ')))
+def append_list(*args):
+    for i in args:
+        list_1.append(i)
 
+
+def append_left_deq(*args):
+    for i in args:
+        deq.appendleft(i)
+
+
+def pop_list():
+    pop = list_1.pop()
+
+
+def popleft_deq():
+    pop = deq.popleft()
+
+
+def extend_list(lst: list):
+    list_1.extend(lst)
+
+
+def extend_left_deque(lst: list):
+    deq.extendleft(lst)
+
+
+def get_item_list(numbers: list):
+    for i in numbers:
+        a = list_1[i]
+
+
+def get_item_deque(numbers: list):
+    for i in numbers:
+        a = deque[i]
+
+
+append_list(1, 2, 3, 4, 5, 6)
+
+print(timeit('append_list(n)', setup='n = (1, 2, 3, 4, 5, 6)', globals=globals()))
+
+append_left_deq(1, 2, 3, 4, 5, 6)
+
+print(timeit('append_left_deq(n)', setup='n = (1, 2, 3, 4, 5, 6)', globals=globals()))
+print()
+# Добавлнение происходит быстрее в списке
+
+print(timeit('pop_list', globals=globals()))
+
+print(timeit('popleft_deq', globals=globals()))
+print()
+# Взятие последнего элемента происходит быстрее в deque
+
+print(timeit('extend_list(n)', setup='n = (7, 8, 9, 10)', globals=globals()))
+
+print(timeit('extend_left_deque(n)', setup='n = (7, 8, 9, 10)', globals=globals()))
+print()
+
+# Объединение происходит быстрее в списке
+
+print(timeit('get_item_list(n)', setup='n = (9, 6, 3, 1)', globals=globals()))
+
+print(timeit('get_item_deque(n)', setup='n = (9, 6, 3, 1)', globals=globals()))
+
+# Получение элементов из списка происходит быстрее
--- task_4.py
+++ task_4.py
@@ -1,11 +1,59 @@
-def sum_n_order(n, elem=1, count=1):
-    if n == 1:
-        print(count)
-        return
-    elem = elem / -2
-    count += elem
-    n -= 1
-    return sum_n_order(n, elem, count)
+"""
+Задача 4.
+Создайте обычный словарь и упорядоченный словарь OrderedDict.
+Выполните операции, равные по смыслу, с каждым из словарей и сделайте замеры.
+Опишите полученные результаты, сделайте выводы
+И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
+"""
+
+from collections import OrderedDict
+
+from timeit import timeit
+
+dict_default = {}
+
+dict_order = OrderedDict()
+
+A = [12, 13, 14, 15, 16]
 
 
-sum_n_order(int(input('Введите длину последовательности: ')))
+def one():
+    for i in range(len(A) - 1, 0, -1):
+        dict_default[i] = A[i]
+
+
+def two():
+    for i in range(len(A) - 1, 0, -1):
+        dict_order[i] = A[i]
+
+
+def three():
+    for i in range(1, 3):
+        del dict_default[i]
+
+
+def four():
+    for i in range(1, 3):
+        del dict_order[i]
+
+
+one()
+print(timeit('one', globals=globals()))
+
+two()
+print(timeit('two', globals=globals()))
+print()
+
+# Быстрее заполняется OrderDict
+
+three()
+print(timeit('three', globals=globals()))
+
+four()
+print(timeit('four', globals=globals()))
+
+# Быстрее удаляется OrderDict
+
+print(dict_default)
+
+print(dict_order)
